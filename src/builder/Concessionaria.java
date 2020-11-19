c278398095b00717104a3799a2514bf6fdc23904
leocassiosilva <leocassiosilva1234@gmail.com>
2020-11-19 17:10:41 -0300

    classe Concecionaria

---
+++ src/builder/Concessionaria.java
@@ -0,0 +1,26 @@
+package builder;
+/**
+ *
+ * @author Leocassio
+ */
+public class Concessionaria {
+    protected MotoBuilder montadora;
+
+    public Concessionaria(MotoBuilder montadora) {
+        this.montadora = montadora;
+    }
+    
+    public void construirMoto(){
+        montadora.buidPreco();
+        montadora.buidAnoDeFabricacao();
+        montadora.buidDscMotor();
+        montadora.buidModelo();
+        montadora.buidMontadora();
+    }
+
+    public MotoBuilder getMontadora() {
+        return montadora;
+    }
+    
+    
+}
