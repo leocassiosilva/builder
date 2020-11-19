0c1fee8e31142991fd3b80638e5141a05d088d95
leocassiosilva <leocassiosilva1234@gmail.com>
2020-11-19 16:58:32 -0300

    classe abstrataProduct

--- src/builder/MotoBuilder.java
+++ src/builder/MotoBuilder.java
@@ -5,5 +5,19 @@ package builder;
  * @author Leocassio
  */
 public abstract class MotoBuilder {
+    protected MotoProduct moto; 
     
+    public MotoBuilder(){
+        moto = new MotoProduct();
+    }
+    
+    public abstract void buidPreco();
+    public abstract void buidDscMotor();
+    public abstract void buidAnoDeFabricacao();
+    public abstract void buidModelo();
+    public abstract void buidMontadora();
+
+    public MotoProduct getMoto() {
+        return moto;
+    }
 }
